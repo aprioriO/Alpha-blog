d996307bb01f5c5d3056bbe0e46c5061e5f10f77
Olena Hulakova <ohulakova@gmail.com>
2020-02-20 21:49:53 +0000

    Add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,13 @@ class UsersController < ApplicationController
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was successfully updated "
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   private
