783a484ae3d498e9739fbdf5bbdff3ff4c16d531
Olena Hulakova <ohulakova@gmail.com>
2020-02-24 01:09:37 +0000

    Add pagination to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 def index
-  # @users = User.paginate(page: params[:page], per_page: 5)
-  @users = User.paginate(page: params[:page], per_page: 4)
+  @users = User.paginate(page: params[:page], per_page: 5)
 end
 
   def new
@@ -35,7 +34,7 @@ end
 
   def show
       @user = User.find(params[:id])
-
+      @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
