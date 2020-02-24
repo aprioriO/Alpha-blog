0430ddd14a6c3e8c72f8ac918f37072788229f82
Olena Hulakova <ohulakova@gmail.com>
2020-02-24 00:48:31 +0000

    Add pagination for users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 def index
-  @users = User.all
+  # @users = User.paginate(page: params[:page], per_page: 5)
+  @users = User.paginate(page: params[:page], per_page: 4)
 end
 
   def new
