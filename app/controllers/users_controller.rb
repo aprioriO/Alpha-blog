21893db887c2619023d02aebbf8b814d7ab3c9af
Olena Hulakova <ohulakova@gmail.com>
2020-02-26 21:59:33 +0000

    Add set user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 def index
   @users = User.paginate(page: params[:page], per_page: 5)
 end
@@ -19,11 +20,10 @@ end
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was successfully updated "
       redirect_to articles_path
@@ -33,12 +33,15 @@ end
   end
 
   def show
-      @user = User.find(params[:id])
       @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+  @user = User.find(params[:id])
   end
 end
