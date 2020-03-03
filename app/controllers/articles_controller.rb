bf5e9d5fe30b7a562a53acdfc6566c699d2ff168
Olena Hulakova <ohulakova@gmail.com>
2020-03-03 20:04:23 +0000

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     # p params
     # debugger
     @article = Article.new(article_params)
-    @article.user = User.first  #to delete after
+    @article.user = current_user  #to delete after
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
