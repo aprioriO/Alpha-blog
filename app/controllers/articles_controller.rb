018769a0e204bc8531fb02c72c4ed446f0989786
Olena Hulakova <ohulakova@gmail.com>
2020-03-05 01:16:43 +0000

    Add admin to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @user
+      if current_user != @user && !current_user.admin
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
       end
--- db/migrate/20200114150205_create_articles.rb
+++ db/migrate/20200114150205_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.text :description
-
       t.timestamps
     end
   end
