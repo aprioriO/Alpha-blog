f82cc7be28431205ac90bf5944ddf8cec41716cb
Olena Hulakova <ohulakova@gmail.com>
2020-03-09 18:38:49 +0000

    Add test for name length

--- test/models/catgory_test.rb
+++ test/models/catgory_test.rb
@@ -22,4 +22,15 @@ class CategoryTest < ActiveSupport::TestCase
     assert_not category2.valid?
   end
 
+  test "name should not be too long" do
+    @category.name = "a"*26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
+
 end
