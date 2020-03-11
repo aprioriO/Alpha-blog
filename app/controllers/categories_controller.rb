6153377d4ecc6b8df37ffcbd44ff4fb859e16cd7
Olena Hulakova <ohulakova@gmail.com>
2020-03-11 19:33:09 +0000

    Define Create method in caregories_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,11 +3,24 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category was created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
