d49eb7052e3548fa1e730698bfa6e9df6f34ea57
Olena Hulakova <ohulakova@gmail.com>
2020-03-11 23:26:37 +0000

    Finalize categories controller test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,22 +1,23 @@
 require 'test_helper'
+class CategoriesControllerTest < ActionController::TestCase
 
-class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @category = Category.create(name: "sports")
+  @category = Category.create(name: "sports")
   end
 
   test "should get categories index" do
-    get categories_path
-    assert_response :success
+  get :index
+  assert_response :success
   end
 
   test "should get new" do
-    get new_category_path
-    assert_response :success
+  get :new
+  assert_response :success
   end
 
   test "should get show" do
-    get category_path(@category)
-    assert_response :success
+  get :show, params:{'id' => @category.id}
+  assert_response :success
   end
+
 end
